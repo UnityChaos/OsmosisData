dc2b2cbeef066e1bf24a298a5fda28107f0a612a
Unity Chaos <unitylchaos@protonmail.com>
2021-09-30 18:32:58 -0700

    update

--- participation.py
+++ participation.py
@@ -32,15 +32,3 @@ validators = {convert_valoper(v["operator_address"]) : int(v["tokens"]) for v in
 get_data = lambda v: [v,True if v in validators else False, sum(delegations[v].values()), validators[v] - sum([delegations[d].get(v,0) for d in delegations]) if v in validators else 0, voters[v]]
 
 export("participation.csv", "\n".join([",".join([str(x) for x in get_data(v)]) for v in voters]))
-
-#shares
-#delegators vote share including self bond
-#validators voting on behalf of delegators
-
-# per validator, get their total voting power
-# 3 values
-# self bond, direct voting delegators, indirect voting for delegators
-
-#then we can compare across validators and total
-
-# address, is_validator, direct vote, indirect vote, vote option
