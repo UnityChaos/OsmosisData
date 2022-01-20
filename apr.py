9da5defa7af38b5f2c90bcb1539afcc35a63c933
Unity Chaos <unitylchaos@protonmail.com>
2022-01-20 10:46:36 -0800

    update

--- apr.py
+++ apr.py
@@ -1,7 +1,6 @@
 import json
 import urllib.request
-
-#Free alpha for anyone who knows how to run "osmosisd export &> state.json"
+import subprocess
 
 j = json.load(open("state.json"))
 
@@ -15,25 +14,24 @@ pids = [denom_to_pid(x) for x in denoms]
 
 pool_total = {denom : int(j["app_state"]["gamm"]["pools"][denom_to_pid(denom)-1]["totalShares"]["amount"]) for denom in denoms}
 
-factor = {denom_to_pid(denom) : {dur/86400: sum([int(x["coins"][0]["amount"]) for x in locks if x["coins"][0]["denom"] == denom and float(x["duration"][:-1])>=dur])/pool_total[denom] for dur in [86400,604800,1209600]} for denom in denoms}
+factor = {denom_to_pid(denom) : {dur/86400: sum([int(x["coins"][0]["amount"]) for x in locks if x["coins"][0]["denom"] == denom and float(x["duration"][:-1])>=dur])/pool_total[denom] for du>
 
 get_aprs = lambda x: {d : sum([float(y["apr_"+str(d)+"d"]) for y in x["apr_list"]]) for d in [1,7,14]}
 
 ui_aprs = {x["pool_id"] : get_aprs(x) for x in json.loads(urllib.request.urlopen(urllib.request.Request("https://api-osmosis.imperator.co/apr/v1/all")).read().decode('utf-8'))}
 
-real_aprs = {pid : {d : ui_aprs[pid][d]/factor[pid][d] for d in [1,7,14]} for pid in ui_aprs.keys()}
+nom_apr = {}
+for pid in ui_aprs.keys():
+  b = {}
+  b[1] = ui_aprs[pid][1]/factor[pid][1]
+  b[7] = b[1] + (ui_aprs[pid][7]-ui_aprs[pid][1])/factor[pid][7]
+  b[14] = b[7] + (ui_aprs[pid][14]-ui_aprs[pid][7])/factor[pid][14]
+  nom_apr[pid] = b
 
 def export(fn, csv):
   f = open(fn, "w")
   f.write(csv)
   f.close()
 
-export("real_apr.csv","\n".join([str(p)+","+",".join([str(real_aprs[p][d]) for d in [1.0,7.0,14.0]]) for p in ui_aprs.keys()]))
+export("nom_apr.csv","\n".join([str(p)+","+",".join([str(nom_apr[p][d]) for d in [1.0,7.0,14.0]]) for p in ui_aprs.keys()]))
 
-gauges = j["app_state"]["incentives"]["gauges"]
-
-external = [",".join([g["coins"][0]["amount"],g["coins"][0]["denom"],g["distribute_to"]["denom"],g["distribute_to"]["duration"],g["filled_epochs"],g["id"],g["num_epochs_paid_over"],g["start_time"]]) for g in gauges if g["is_perpetual"]==False]
-
-
-
-export("external_gauges.csv","\n".join(external))
